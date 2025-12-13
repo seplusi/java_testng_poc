fe5cdb667da3e23bebabdfaf145581ba6ca1e014
Luis <seplusi_arcanjo@hotmail.com>
2025-12-13 15:22:57 -0800

    first commit

---
+++ demo/src/test/java/LoginTest.java
@@ -0,0 +1,34 @@
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    WebDriver driver;
+    String url = "https://opensource-demo.orangehrmlive.com/";
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test(groups = {"fast"})
+    public void aFastTest() {
+        System.out.println("Fast test");
+        driver.get(url);
+        System.out.println("Finished test");
+    }
+
+    @Test(groups = {"slow"})
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+
+}
+
